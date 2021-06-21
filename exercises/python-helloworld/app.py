7c32da71261cd9e16b3039d23518b9506edee938
Slava Fomin II <slava@fomin.io>
2021-06-21 11:54:04 +0300

    Introduced status and metrics endpoints
    Signed-off-by: Slava Fomin II <slava@fomin.io>

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World"
+
+
+@app.route("/status")
+def status():
+    return {
+        "result": "OK — Healthy"
+    }
+
+
+@app.route("/metrics")
+def metrics():
+    return {
+        "UserCount": 140,
+        "UserCountActive": 23,
+    }
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
