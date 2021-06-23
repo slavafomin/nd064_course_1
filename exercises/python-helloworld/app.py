411d85b88d9913901ca95b94171c045d72d18857
Slava Fomin II <slava@fomin.io>
2021-06-23 15:51:36 +0300

    Introduced logging
    Signed-off-by: Slava Fomin II <slava@fomin.io>

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,22 +1,36 @@
+
+import logging
+
 from flask import Flask
+
+
+logging.basicConfig(
+    format='[%(asctime)s] [%(levelname)s]: %(message)s',
+    filename='app.log',
+    encoding='utf-8',
+    level=logging.DEBUG
+)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+    logging.debug('Home endpoint reached')
     return "Hello World"
 
 
 @app.route("/status")
 def status():
+    logging.debug('Status endpoint reached')
     return {
-        "result": "OK — Healthy"
+        "result": "OK"
     }
 
 
 @app.route("/metrics")
 def metrics():
+    logging.debug('Metrics endpoint reached')
     return {
         "UserCount": 140,
         "UserCountActive": 23,
